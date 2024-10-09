a68333fc965d0eddba3b077fe4ea44d3830fbfb0
14382 <14382@gmail.com>
2024-10-09 18:42:03 -0400

    message

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniWheelTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OmniWheelTeleOp.java
@@ -11,7 +11,7 @@ public class OmniWheelTeleOp extends LinearOpMode {
     DcMotor rightFrontDrive = null;
     DcMotor rightBackDrive = null;
 
-    private static final double TURN_SPEED_FACTOR = 0.8;
+    private static final double TURN_SPEED_FACTOR = 0.7;
     private static final double ACCELERATION_RATE = 0.008; // Change in power per update
     private static final double strafe_speed = 0.8;
     private static final double SPEED_MULTIPLIER = 1; // Adjust this value to change speed
